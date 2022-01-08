d17f7829c947ad137c08f3ff0c7b0549948fbaf4
Park Min Hyeok <pmh_only@pmh.codes>
2022-01-09 00:44:01 +0900

    feat: add tutorial stage

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -17,16 +17,20 @@ public class PlayerScript : MonoBehaviour {
   }
 
   void Update () {
+    if (Input.GetKeyDown(KeyCode.R)) respawn();
+
+    if (jumped > 1) GetComponent<Renderer>().material.color = new Color32(129, 13, 255, 255);
+    else if (jumped > 0) GetComponent<Renderer>().material.color = new Color32(178, 12, 232, 255);
+    else GetComponent<Renderer>().material.color = new Color32(255, 0, 242, 255);
+
     if (jumped > 1) return;
 
     if (Input.GetKeyDown(KeyCode.Space)) {
-      body.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
-      jumped++;
+        body.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+        jumped++;
     }
 
-    if (Input.GetKeyDown(KeyCode.R)) respawn();
-
-    float horizontalAxis = Input.GetAxis("Horizontal");
+    float horizontalAxis = Input.GetAxis("Horizontal") * (SceneManager.GetActiveScene().buildIndex == 0 ? 2 : 1);
     float verticalAxis = Input.GetAxis("Vertical");
     body.velocity = new Vector3(horizontalAxis, body.velocity.y, verticalAxis);
   }
